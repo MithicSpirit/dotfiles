2958bb813677c89bebdf6f1c3fc86fe8b0b73e6d
MithicSpirit <rpc01234@gmail.com>
2022-01-08 17:58:27 -0500

    wallpapers: change used wallpaper

--- .config/qtile/scripts/autostart.sh
+++ .config/qtile/scripts/autostart.sh
@@ -12,7 +12,7 @@
 	# Appearance
 	sleep 1
 	picom --experimental-backends &
-	feh --no-fehbg --bg-fill "$HOME/.wallpapers/snowy mountains.jpg" &
+	feh --no-fehbg --bg-fill "$HOME/.wallpapers/planets.jpg" &
 	redshift-gtk -l geoclue2 -b 0.95:0.9 -t 6400K:4000K &
 	xrdb ~/.config/xresources/Xresources &
 
