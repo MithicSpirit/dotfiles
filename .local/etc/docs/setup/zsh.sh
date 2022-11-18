f6abc8c04ed83c0dc516764b5dcd5e7878ee9bc7
MithicSpirit <rpc01234@gmail.com>
2022-11-17 23:13:17 -0500

    docs(setup): i am retarded

--- .local/etc/docs/setup/zsh.sh
+++ .local/etc/docs/setup/zsh.sh
@@ -8,4 +8,4 @@ safeclone https://github.com/zsh-users/zsh-autosuggestions.git "$PLUGINPATH/zsh-
 safeclone https://github.com/zsh-users/zsh-completions.git "$PLUGINPATH/zsh-completions"
 safeclone https://github.com/zsh-users/zsh-history-substring-search.git "$PLUGINPATH/zsh-history-substring-search"
 safeclone https://github.com/zsh-users/zsh-syntax-highlighting.git "$PLUGINPATH/zsh-syntax-highlighting"
-safeclone https://github.com/romkatv/powerlevel10k.git "$PLUGINPATH/zsh-syntax-highlighting"
+safeclone https://github.com/romkatv/powerlevel10k.git "$PLUGINPATH/powerlevel10k"
