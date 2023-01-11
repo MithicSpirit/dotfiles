c7b8967c12adcff5cb4268fed4476ce5bde0290f
MithicSpirit <rpc01234@gmail.com>
2023-01-10 22:57:16 -0500

    docs(setup): escape URLs

--- .local/etc/docs/setup/zsh.sh
+++ .local/etc/docs/setup/zsh.sh
@@ -4,8 +4,8 @@
 
 PLUGINPATH="$HOME/.config/zsh/plugins"
 
-safeclone "$PLUGINPATH/zsh-autosuggestions" https://github.com/zsh-users/zsh-autosuggestions.git
-safeclone "$PLUGINPATH/zsh-completions" https://github.com/zsh-users/zsh-completions.git
-safeclone "$PLUGINPATH/zsh-history-substring-search" https://github.com/zsh-users/zsh-history-substring-search.git
-safeclone "$PLUGINPATH/zsh-syntax-highlighting" https://github.com/zsh-users/zsh-syntax-highlighting.git
-safeclone "$PLUGINPATH/powerlevel10k" https://github.com/romkatv/powerlevel10k.git
+safeclone "$PLUGINPATH/zsh-autosuggestions" 'https://github.com/zsh-users/zsh-autosuggestions.git'
+safeclone "$PLUGINPATH/zsh-completions" 'https://github.com/zsh-users/zsh-completions.git'
+safeclone "$PLUGINPATH/zsh-history-substring-search" 'https://github.com/zsh-users/zsh-history-substring-search.git'
+safeclone "$PLUGINPATH/zsh-syntax-highlighting" 'https://github.com/zsh-users/zsh-syntax-highlighting.git'
+safeclone "$PLUGINPATH/powerlevel10k" 'https://github.com/romkatv/powerlevel10k.git'
