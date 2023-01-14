16ac5544779b79060549155caf7b9b20c5554d14
MithicSpirit <rpc01234@gmail.com>
2023-01-14 15:58:16 -0500

    qtile(autostart): restart emacs, cleanup, tmux

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -26,9 +26,10 @@ setbackground &
 
 sleep 0.5
 kded5 &
-#systemctl --user restart emacs.service &
+systemctl --user restart emacs.service &
 xautolock &
 greenclip daemon >>/tmp/greenclip.log &
+tmux -D &
 redshift-gtk -l geoclue2 -b 0.95:0.9 -t 6400K:4000K &
 unkrun kdeconnect-indicator &
 unkrun flameshot &
@@ -48,33 +49,15 @@ nice -n5 discord-canary >>/tmp/discord.log 2>&1 &
 nice -n5 signal-desktop >>/tmp/signal.log 2>&1 &
 nice -n5 element-desktop >>/tmp/element.log 2>&1 &
 
-#nice -n5 evolution >>/tmp/evolution.log 2>&1 &
 nice -n5 kmail >>/tmp/kmail.log 2>&1 &
 nice -n5 kalendar >>/tmp/kalendar.log 2>&1 &
-#{
-#	{
-#		echo "RMing"
-#		rm -rf "$XDG_DATA_HOME/akonadi/file_db_data"
-#		rm -rf "$XDG_DATA_HOME/akonadi/file_lost+found"
-#		echo "Starting akonadi"
-#		akonadictl start
-#		sleep 1.0
-#		echo "Running vacuum"
-#		akonadictl vacuum
-#		echo "Running fsck"
-#		akonadictl fsck
-#	} >>/tmp/akonadi.log 2>&1
-#
-#	nice -n5 kmail >>/tmp/kmail.log 2>&1 &
-#	nice -n5 kalendar >>/tmp/kalendar.log 2>&1 &
-#} &
 nice -n5 lbry >>/tmp/lbry.log 2>&1 &
 # shellcheck disable=SC2086
 nice -n5 $TERMINAL --class btop-spawn -t btop -e btop \
 	>>/tmp/alacritty-btop.log 2>&1 &
 
 sleep 2.0
-(akonadictl vacuum; akonadictl fsck) &
+{akonadictl vacuum; akonadictl fsck} &
 killall dunst &
 dunst -print >>/tmp/dunst.log &
 sleep 0.1
